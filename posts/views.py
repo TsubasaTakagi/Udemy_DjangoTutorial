f0618e07d0d09fbbee1e6b0555165dff5caca394
TsubasaTakagi <mizuirotech@gmail.com>
2017-12-31 08:49:09 +0900

    image

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello World! このページは投稿のインデックスです。")
+    # return HttpResponse("Hello World! このページは投稿のインデックスです。")
+    posts = Post.objects.order_by('-published')
+    return render(request, 'posts/index.html', {'posts': posts})
+
+def post_detail(request, post_id):
+    return render(request, 'posts/post_detail.html', {'post_id':post_id})
